8c8a1e4b5b939f5ed8a56a7cfc7bbe2d8235cb28
Crowdbotics <team@crowdbotics.com>
2023-08-03 20:23:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "esm-portal-42625",
+  url: "https://esm-portal-42625.botics.co",
+  api: "https://esm-portal-42625.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
